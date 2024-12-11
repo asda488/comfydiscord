467a8fdc8dbcbd2ef1b2dc75afa05d47d31cfd05
Yoshi Chan <yoshicys@gmail.com>
2024-12-11 02:18:46 +0000

    Refactoring and commands

--- image.py
+++ image.py
@@ -1,20 +1,20 @@
-import websockets.sync.client
+import websockets.asyncio.client
 import json
 import requests
+import aiohttp
 import random
 import uuid
 
-server_address = "192.168.1.69:8188"
 client_id = str(uuid.uuid4())
 
 
-def build_json(text):
+def build_json(text, negative):
     workflow = """
     {
     "3": {
         "inputs": {
-        "seed": 1095405104177859,
-        "steps": 20,
+        "seed": 0,
+        "steps": 25,
         "cfg": 7.00,
         "sampler_name": "dpmpp_2m_sde_gpu",
         "scheduler": "karras",
@@ -40,7 +40,7 @@ def build_json(text):
     },
     "4": {
         "inputs": {
-        "ckpt_name": "meinamix_meinaV11.safetensors"
+        "ckpt_name": "AnythingXL_v50.safetensors"
         },
         "class_type": "CheckpointLoaderSimple"
     },
@@ -88,7 +88,7 @@ def build_json(text):
     "9": {
         "inputs": {
         "images": [
-            "15",
+            "16",
             0
         ]
         },
@@ -138,37 +138,61 @@ def build_json(text):
         ]
         },
         "class_type": "ImageUpscaleWithModel"
+    },
+    "16": {
+        "inputs": {
+        "upscale_method": "lanczos",
+        "width": 768,
+        "height": 768,
+        "crop": "disabled",
+        "image": [
+            "15",
+            0
+        ]
+        },
+        "class_type": "ImageScale"
     }
     }
     """
     workflow = json.loads(workflow)
     workflow["6"]["inputs"]["text"] = text
     workflow["3"]["inputs"]["seed"] = random.getrandbits(64)
+    workflow["7"]["inputs"]["text"] = negative
     return {"prompt": workflow, "client_id": client_id}
 
-def send_prompt(prompt):
+#takes json prompt, returns int prompt_id
+async def send_prompt(server_address, prompt):
     data = json.dumps(prompt).encode('utf-8')
-    return requests.post(f"http://{server_address}/prompt", data=data)
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f'http://{server_address}/prompt', data=data) as resp:
+            body = await resp.text()
+    return json.loads(body)["prompt_id"]
 
-def listen_image(ws, id):
-    while True:
-        out = ws.recv()
-        if isinstance(out, str):
-            message = json.loads(out)
-            if message['type'] == 'executing':
-                data = message['data']
-                if "prompt_id" in data and data['prompt_id'] == id:
-                    if data['node'] is None:
-                        break #Execution is done
-                    else:
-                        current_node = data['node']
-        else:
-            if current_node == "9": #save_image_websocket_node
-                image_data = out[8:]
-    return image_data
+async def listen_image(server_address, id):
+    async with websockets.asyncio.client.connect(f"ws://{server_address}/ws?clientId={client_id}", max_size=None, ping_interval=None) as websocket: #not expecting more than 32MB
+        current_node = 0
+        async for msg in websocket:
+            if isinstance(msg, str):
+                message = json.loads(msg)
+                if message['type'] == 'executing':
+                    data = message['data']
+                    if "prompt_id" in data and data['prompt_id'] == id:
+                        if data['node'] is None:
+                            return image_data #Execution is done
+                        else:
+                            current_node = data['node']
+            else:
+                if current_node == "9": #save_image_websocket_node
+                    image_data = msg[8:]
 
-def gen_image(text):
-    prompt = build_json(text)
-    id = send_prompt(prompt).json()["prompt_id"]
-    with websockets.sync.client.connect(f"ws://{server_address}/ws?clientId={client_id}", max_size=2**25) as websocket: #not expecting more than 32mb
-        return listen_image(websocket, id), id
+async def gen_image(server_address, text, negative=None, anime=False):
+    if anime:
+        positive = "masterpiece, (best quality), newest, recent, 1girl, extreme detailed, " + text
+        neg = "embedding:EasyNegative.pt,(worst quality),(low quality),(normal quality), text, watermark, lowres, (bad anatomy), (bad hands), error, missing fingers,extra digit,fewer digits,cropped,jpeg artifacts,signature,watermark,username,blurry, old,furry," + (negative if negative else "")
+    else:
+        positive = "masterpiece, (best quality), " + text
+        neg = "embedding:EasyNegative.pt,(worst quality),(low quality),(normal quality), text, watermark, lowres"
+    prompt = build_json(positive, negative)
+    id = await send_prompt(server_address, prompt)
+    image = await listen_image(server_address, id)
+    return image, id
--- main.py
+++ main.py
@@ -1,36 +1,89 @@
 import discord
+from discord.ext import commands, tasks
 from image import gen_image
 import io
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='?', intents=intents)
 
 generating = False
 
-id = 0 #channel id
+server_address = "127.0.0.1:8188"
 token = None #discord token
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    cmd_tree = await client.tree.sync()
+    print(f"cmds sync'd> {str(len(cmd_tree))}")
+    print(f"Logged in as {client.user}")
 
-@client.event
-async def on_message(message):
-    global generating
-    if message.channel.id == id and message.author != client.user and client.user.mentioned_in(message):
+# reply with the button
+class ImageView(discord.ui.View):
+    def __init__(self, prompt, negative, model=None, image_count=1, anime=False):
+        super().__init__()
+        self.prompt = prompt
+        self.negative = negative
+        self.model = model
+        self.image_count = image_count
+        self.anime = anime
+
+    @discord.ui.button(label="Generate Again", style=discord.ButtonStyle.primary)
+    async def generate_again(self, button_interaction: discord.Interaction, button: discord.ui.Button):
+        global generating
         if not generating:
-            generating = True
-            text = message.content.replace(f"<@{client.user.id}>", "")
-            print(message.content)
-            await message.channel.send(f"Generating with prompt \"{text}\" (please wait 1-2 minutes)")
-            image_bytes, name = gen_image(text)
-            await message.channel.send(file=discord.File(io.BytesIO(image_bytes), filename=f"{name}.png", spoiler=True))
-            generating = False
-        """
-        else:
-            message.channel.send(f"Currently generating, your prompt will be ignored, please try again after current generation")
-        """
+            try:
+                generating = True
+                await button_interaction.response.defer(thinking=True)
 
-client.run('token')
+                print("Image generation queued, prompt: {self.prompt}, negative: {self.negative}, anime: {self.anime}")
+
+                image_bytes, name = await gen_image(server_address, self.prompt, self.negative, self.anime)
+
+                reply_view = ImageView(self.prompt, self.negative, self.model, self.image_count, self.anime)
+                
+                await button_interaction.followup.send(file=discord.File(io.BytesIO(image_bytes), filename=f"{name}.png", spoiler=True), view=reply_view)
+                generating = False
+                
+            except Exception as e:
+                # ephemeral so no one else sees it
+                generating = False
+                await button_interaction.response.send_message(f"An error occured: {e}", ephemeral=True)
+        
+        else:
+            await button_interaction.response.send_message(f"Currently generating, your prompt will be ignored, please try again after current generation", ephemeral=True)
+    
+@client.tree.command(name="generate", description="generates image")
+async def generate(interaction: discord.Interaction, prompt:str, negative:str=None, image_count:int=1, model:str=None, anime:bool=False):
+    global generating
+    if not generating:
+        try:
+            generating = True
+            await interaction.response.defer(thinking=True)
+            
+            print("Image generation queued, prompt: {prompt}, negative: {negative}, anime: {anime}")
+
+            image_bytes, name = await gen_image(server_address, prompt, negative, anime)
+
+            image_view = ImageView(prompt, negative, model, image_count, anime)
+            
+            await interaction.followup.send(file=discord.File(io.BytesIO(image_bytes), filename=f"{name}.png", spoiler=True), view=image_view)
+            
+            generating = False
+            
+        except Exception as e:
+            generating = False
+            await interaction.response.send_message(f"An error occured: {e}")
+    
+    else:
+        await interaction.response.send_message(f"Currently generating, your prompt will be ignored, please try again after current generation")
+
+@client.tree.command(name="backend", description="switch server backend")
+async def backend(interaction: discord.Interaction, url:str):
+    global server_address
+    server_address = url
+    await interaction.response.send_message(f"Server backend switched to {url}")
+    print(f"Server backend switched to {url}")
+
+client.run(token)
